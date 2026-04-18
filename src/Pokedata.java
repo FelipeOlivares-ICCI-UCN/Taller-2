6e1b15a99a715d1982b7dd600004f099de6db8f9
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-04-18 14:25:32 -0400

    Added Pokedata class with initalizer and setStats method.

---
+++ src/Pokedata.java
@@ -0,0 +1,45 @@
+public class Pokedata {
+    private String name;
+
+    private String location;
+    private float apparitionChance;
+
+    private int healthPoints;
+    private int attackPoints;
+    private int defensePoints;
+    private int specialAttackPoints;
+    private int specialDefensePoints;
+    private int speedPoints;
+
+    private int totalStats;
+    private String type;
+
+
+    public Pokedata(String name, String location, float apparitionChance, String type)
+    {
+        this.name = name;
+        this.location = location;
+        this.apparitionChance = apparitionChance;
+        this.type = type;
+
+    }
+
+    public void setStats(int HP, int AP, int DP, int sAP, int sDP, int SP)
+    {
+        this.healthPoints = HP;
+        this.attackPoints = AP;
+        this.defensePoints = DP;
+        this.specialAttackPoints = sAP;
+        this.specialDefensePoints = sDP;
+        this.speedPoints = SP;
+
+        this.totalStats = (HP + AP + DP
+                + sAP + sDP
+                + SP);
+
+    }
+
+
+
+
+}
