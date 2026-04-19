7ea8b0cee76ec47e0a681d3e1abd385e8f5f8624
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-04-19 15:34:31 -0400

    Added new Pokemon class

---
+++ src/Pokemon.java
@@ -0,0 +1,55 @@
+public class Pokemon {
+    private String name;
+    private int totalStats;
+    private String type;
+    private boolean isKnocked;
+
+    public Pokemon(String name, int totalStats, String type)
+    {
+        this.name = name;
+        this.totalStats = totalStats;
+        this.type = type;
+    }
+
+    public String getState()
+    {
+        if (!isKnocked)
+        {
+            return "Vivo";
+        }
+
+        else
+        {
+            return "Debilitado";
+        }
+
+
+    }
+
+    public void setState(String state)
+    {
+        if (state.equals("Vivo"))
+        {
+            this.isKnocked = false;
+        }
+
+        else if (state.equals("Debilitado"))
+        {
+            this.isKnocked = true;
+        }
+
+        else
+        {
+            System.out.println("Estado de pokemon invalido");
+        }
+
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return this.name + "|" + this.totalStats + "|" + this.type + "|" + this.getState();
+    }
+}
