5543b84c1cdf3ed11d6f7c063d2d437c1841b816
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-04-24 10:56:25 -0400

    Added GymManager wrapper class

---
+++ src/GymManager.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GymManager {
+    List<GymData> GymList = new ArrayList<>();
+
+
+    public GymManager(int numMedals)
+    {
+        try (Scanner fileReader = new Scanner( new File("./data/Gimnasios.txt")))
+        {
+            while (fileReader.hasNextLine())
+            {
+                String[] data = fileReader.nextLine().split(";");
+
+                int gymNumber = Integer.parseInt(data[0]);
+                String gymName = data[1];
+                boolean defeatState = false;
+
+                if (numMedals >= gymNumber)
+                {
+                    defeatState = true;
+                }
+
+
+                int teamSize = Integer.parseInt(data[3]);
+
+                String[] pokemonNames = GymList.subList(4, 4 + teamSize).toArray(new String[teamSize]);
+
+                GymList.add(new GymData(gymNumber, gymName, defeatState, pokemonNames));
+
+
+
+            }
+
+
+        } catch (FileNotFoundException e)
+        {
+            throw new RuntimeException("No se encontro el archivo Gimnasios.txt", e);
+        }
+    }
+
+
+}
